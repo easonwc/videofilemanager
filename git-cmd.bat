fe6dcaeceebd8cc6b12044ab41176681e2b0475c
William C Eason II <willeason2@gmail.com>
2026-04-09 13:27:54 -0400

    v1.4.0-installer-rebuild

--- git-cmd.bat
+++ git-cmd.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\kiroProjects\videoFileManager\video-manager
 git add .
-git commit -m v1.4.0-enhanced-player-and-duplicates
+git commit -m v1.4.0-installer-rebuild
 git push origin master
 echo Done!
