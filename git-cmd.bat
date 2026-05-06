6234b72649601a390b37f2346b59ccf5199900ab
William C Eason II <willeason2@gmail.com>
2026-05-06 16:43:31 -0400

    v1.6.0-performance-fixes

--- git-cmd.bat
+++ git-cmd.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d C:\kiroProjects\videoFileManager\video-manager
 git add .
-git commit -m v1.6.0-video-clarity-and-metadata-cache
+git commit -m v1.6.0-performance-fixes
 git push origin master
 npm run build
 echo Done!
